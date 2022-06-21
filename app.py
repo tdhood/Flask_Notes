c160d2119c6bb9f92dced72d2a0ffc5cafd4bfdb
tdhood <taylordannhood@gmail.com>
2022-06-21 13:36:24 -0700

    saved

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 """Flask app for notes."""
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, render_template, flash
 from models import db, connect_db, User
+from forms import AddUserForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///notebooks'
@@ -19,3 +20,26 @@ def root():
 
     return redirect('/register')
 
+@app.route("/register", methods=["GET", "POST"])
+def show_form_and_handle_create_user():
+    """Load form; handle creating user"""
+
+    form = AddUserForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        user = User(username=username,
+        password=password,
+        email=email,
+        first_name=first_name,
+        last_name=last_name)
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect('/secret')
